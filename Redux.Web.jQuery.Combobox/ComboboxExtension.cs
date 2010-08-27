630501c7a3480e987e4cbe7e430911f2f43f68db
RossMerr <email@rossmerrigan.com>
2010-08-27 14:59:31 +0100

    updated Extension

--- Redux.Web.jQuery.Combobox/ComboboxExtension.cs
+++ Redux.Web.jQuery.Combobox/ComboboxExtension.cs
@@ -5,15 +5,30 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using Redux.Web.Templating;
 
 namespace Redux.Web.jQuery.Combobox
 {
     public static class ComboboxExtension
     {
-        public static void ComboboxFor(this HtmlHelper htmlHelper, string name, IEnumerable<SelectListItem> collection)
+        public static string Combobox(this HtmlHelper htmlHelper, string name, IEnumerable<SelectListItem> collection)
         {
             htmlHelper.ViewContext.Writer.WriteLine(htmlHelper.DropDownList(name, collection));
             ComboboxWriter.Writer(htmlHelper, name);
+            return string.Empty;
+        }
+
+        public static string ComboboxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, 
+            IEnumerable<SelectListItem> collection)
+        {
+            var name = ExpressionHelper.GetExpressionText(expression);
+            name = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
+            name = Identifier.GenerateId(name);
+
+            
+            htmlHelper.ViewContext.Writer.WriteLine(htmlHelper.DropDownList(name, collection));
+            ComboboxWriter.Writer(htmlHelper, name);
+            return string.Empty;
         }
     }
 }
