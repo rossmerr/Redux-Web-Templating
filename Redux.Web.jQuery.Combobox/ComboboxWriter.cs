6c177d28769dd3b2c72a5c13d31f4a3d19c2f57e
RossMerr <email@rossmerrigan.com>
2010-08-27 13:55:34 +0100

    combobox

---
+++ Redux.Web.jQuery.Combobox/ComboboxWriter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using Redux.Web.Templating;
+
+namespace Redux.Web.jQuery.Combobox
+{
+    public class ComboboxWriter
+    {
+        public static void Writer(HtmlHelper htmlHelper, string target)
+        {
+            using (var script = new TemplateScript(htmlHelper.ViewContext, htmlHelper.ViewContext.Writer, htmlHelper.ViewContext.FormContext))
+            {
+                script.WriteLine(string.Format("$('#{0}').{1} (", target, Resources.Options.Combobox));
+                script.WriteLine("$(\"#toggle\").click(function() {");
+                script.WriteLine(string.Format("$(\"#{0}\").toggle();", target));
+                script.WriteLine("});");
+                script.WriteLine(");");
+            }
+        }
+    }
+}
