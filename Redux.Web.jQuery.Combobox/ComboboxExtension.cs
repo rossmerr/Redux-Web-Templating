7fbde8e23676247c01c661ac66757712221f18b8
RossMerr <email@rossmerrigan.com>
2010-11-24 14:18:46 +0000

    added html attribute to combobox

--- Redux.Web.jQuery.Combobox/ComboboxExtension.cs
+++ Redux.Web.jQuery.Combobox/ComboboxExtension.cs
@@ -5,6 +5,7 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Web.Mvc;
 using System.Web.Mvc.Html;
+using System.Web.Routing;
 using Redux.Web.Html;
 using Redux.Web.Templating;
 
@@ -22,14 +23,32 @@ namespace Redux.Web.jQuery.Combobox
         public static string ComboboxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression, 
             IEnumerable<SelectListItem> collection)
         {
+            return htmlHelper.ComboboxFor(expression, collection,
+                                 (IDictionary<string, object>)new RouteValueDictionary());
+        }
+
+        public static string ComboboxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression,
+            IEnumerable<SelectListItem> collection, object htmlAttributes)
+        {
+            return htmlHelper.ComboboxFor(expression, collection,
+                                 (IDictionary<string, object>)new RouteValueDictionary(htmlAttributes));
+        }
+
+        public static string ComboboxFor<TModel, TValue>(this HtmlHelper<TModel> htmlHelper, Expression<Func<TModel, TValue>> expression,
+            IEnumerable<SelectListItem> collection, IDictionary<string, object> htmlAttributes)
+        {
             var name = ExpressionHelper.GetExpressionText(expression);
             name = htmlHelper.ViewContext.ViewData.TemplateInfo.GetFullHtmlFieldName(name);
             name = Identifier.GenerateId(name);
-           
-            SelectWriter.Writer(htmlHelper, collection, new {Name = name, id = name});
+
+            htmlAttributes.Add("Name", name);
+            htmlAttributes.Add("Id", name);
+
+            SelectWriter.Writer(htmlHelper, collection, htmlAttributes);
             ComboboxWriter.Writer(htmlHelper, name);
             return string.Empty;
         }
+
 
         public static string ComboboxApi(this HtmlHelper htmlHelper)
         {
